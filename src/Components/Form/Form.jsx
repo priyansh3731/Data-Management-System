259e77be8afa7da60747f12ec85b3b4d3de58680
priyansh3731 <118509082+priyansh3731@users.noreply.github.com>
2023-09-07 09:00:02 +0530

    hello

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -52,6 +52,7 @@ const Form = () => {
       const wsname = wb.SheetNames[0];
       const ws = wb.Sheets[wsname];
       const data = XLSX.utils.sheet_to_json(ws);
+      console.log(data)
       data.map((data)=>(axios.post("https://grumpy-jacket-lamb.cyclic.app/data",data)))
     navigate("/")
     }
